e6eff084b9b3008876ad64392536e56fadf4aee2
ming-hu427 <56187830+ming-hu427@users.noreply.github.com>
2020-10-12 15:20:34 +0800

    Update hello_world_test.cc

--- tensorflow/lite/micro/examples/hello_world/hello_world_test.cc
+++ tensorflow/lite/micro/examples/hello_world/hello_world_test.cc
@@ -14,11 +14,11 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/lite/micro/all_ops_resolver.h"
-# 訓練好且經過轉換為C檔案的模型
+// 訓練好且經過轉換為C檔案的模型
 #include "tensorflow/lite/micro/examples/hello_world/model.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
-# 測試用的輕量級框架
+// 測試用的輕量級框架
 #include "tensorflow/lite/micro/testing/micro_test.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
